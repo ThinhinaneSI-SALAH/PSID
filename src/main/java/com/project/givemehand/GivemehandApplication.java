466c32174bdfe90812e913bee8a7e55a7080ae2f
SidibeYoro <sidib.yoro@yahoo.fr>
2021-03-12 21:25:42 +0100

    V0

---
+++ src/main/java/com/project/givemehand/GivemehandApplication.java
@@ -0,0 +1,15 @@
+package com.project.givemehand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivemehandApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(GivemehandApplication.class, args);
+		System.out.println("bonjour");
+	}
+
+}
---
+++ src/test/java/com/project/givemehand/GivemehandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.givemehand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivemehandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
