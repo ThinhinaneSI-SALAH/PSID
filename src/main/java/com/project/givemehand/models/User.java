702e91b8945a3cd5430d6db78d501523cf3e0920
ThinhinaneSI-SALAH <46303339+T-MIAGE@users.noreply.github.com>
2021-04-07 13:11:42 +0200

    Package services and repository

--- src/main/java/com/project/givemehand/models/User.java
+++ src/main/java/com/project/givemehand/models/User.java
@@ -8,4 +8,14 @@ public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private int medailles;
+
+    public User(){
+
+    }
 }
