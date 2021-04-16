20d57bf1f6164586c7901c47c8b3f1606a4abf3b
diagneAwa <mrsdiagne97@gmail.com>
2021-04-16 17:58:24 +0200

    Merge + error correction - V2.0

---
+++ src/main/java/com/project/givemehand/frontend/givemehand/src/app/register/register.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { User } from '../classes/user';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  user : User = new User();;
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    this.authService.register(this.user).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
+
+}
---
+++ src/main/java/com/project/givemehand/frontend/givemehand/src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../classes/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private  baseUrl = environment.api_url;
+
+  constructor(private http: HttpClient) { }
+
+  public  updateUser(email: string, user: User) : Observable<User>{
+    return this.http.put<User>(`${this.baseUrl}/auth/updateUser/${email}`,user);
+  }
+
+  public getUserByEmail(email: string){
+    return this.http.get(`${this.baseUrl}/auth/findUserByemail/${email}`);
+  }
+
+}
