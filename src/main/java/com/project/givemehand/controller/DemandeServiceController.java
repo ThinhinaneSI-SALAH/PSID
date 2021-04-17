bdeb51438abf9c2bd71449067f397b85e1d82486
kamis <sadaouikamilia@gmail.com>
2021-04-17 13:44:03 +0200

    add request service request function to reload requests

--- src/main/java/com/project/givemehand/controller/DemandeServiceController.java
+++ src/main/java/com/project/givemehand/controller/DemandeServiceController.java
@@ -2,7 +2,6 @@ package com.project.givemehand.controller;
 
 import com.project.givemehand.models.entity.*;
 import com.project.givemehand.services.RequestService;
-import jdk.jshell.Snippet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
