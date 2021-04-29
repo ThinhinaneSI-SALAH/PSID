80c1b3491957668db5c6c10275323f1c1919ad56
ThinhinaneSI-SALAH <80406543+ThinhinaneSI-SALAH@users.noreply.github.com>
2021-04-30 00:25:47 +0200

    Update GivemehandApplicationTests.java

--- src/test/java/com/project/givemehand/GivemehandApplicationTests.java
+++ src/test/java/com/project/givemehand/GivemehandApplicationTests.java
@@ -3,7 +3,7 @@ package com.project.givemehand;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(class=GivemehandApplicationTests.class)
+@SpringBootTest(classes =GivemehandApplicationTests.class)
 class GivemehandApplicationTests {
 
 	@Test
